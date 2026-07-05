00d01c885ed9e5f3ff2bb3a491916e784af0edaf
AhmedArafa7 <mo1999382@gmail.com>
2026-07-05 06:54:54 +0300

    fix: add cache buster to bypass service worker cache for game iframe

--- src/app/features/arcade/arcade.service.ts
+++ src/app/features/arcade/arcade.service.ts
@@ -26,7 +26,7 @@ export class ArcadeService {
       thumbnail: 'assets/images/tank-battle-thumb.jpg', // We can use a placeholder
       genre: 'Action',
       platforms: ['browser'],
-      localUrl: '/games/tank-battle/index.html',
+      localUrl: '/games/tank-battle/index.html?v=2',
       status: 'available'
     },
     {
