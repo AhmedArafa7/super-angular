790c59a144a2ad6600ee23790d5cd3320dcf503d
AhmedArafa7 <mo1999382@gmail.com>
2026-09-09 07:53:50 +0300

    fix: remove non-existent loadAllBookmarks call

--- src/app/features/local-player/local-player.component.ts
+++ src/app/features/local-player/local-player.component.ts
@@ -1215,7 +1215,6 @@ export class LocalPlayerComponent implements OnInit, OnDestroy {
     this.loadUserPreferences();
     this.checkStorageQuota();
     await this.restoreStoredPlaylist();
-    await this.loadAllBookmarks();
     await this.loadRecycleBinAndState();
     await this.refreshNotesCounts();
   }
